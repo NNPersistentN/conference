34dceb402f77e0c10747e0ce3a5518238a07a2ce
NNPersistentN <1210693421@qq.com>
2018-04-22 22:31:06 +0800

    'as'

--- js/index.js
+++ js/index.js
@@ -1,54 +1,64 @@
-var width = document.documentElement.clientWidth;
-var height =  document.documentElement.clientHeight;
-if( width < height ){
-    $print =  $('.item');
-    $print.width(height);
-    $print.height(width);
-    $print.css('top',  (height-width)/2 );
-    $print.css('left',  0-(height-width)/2 );
-    $print.css('transform' , 'rotate(90deg)');
-    $print.css('transform-origin' , '50% 50%');
-}
+// var width = document.documentElement.clientWidth;
+// var height =  document.documentElement.clientHeight;
+// if( width < height ){
+//     $print =  $('.item');
+//     $print.width(height);
+//     $print.height(width);
+//     $print.css('top',  (height-width)/2 );
+//     $print.css('left',  0-(height-width)/2 );
+//     $print.css('transform' , 'rotate(90deg)');
+//     $print.css('transform-origin' , '50% 50%');
+// }
 $(function(){
-    function abc(){
-        num++;
-        if(num>=57){
-            clearInterval(t);
-        }
-        if(0<num&&num<=6){
+    $(".loading").eq(0).addClass("active")
+    setTimeout(function(){
+        $(".loading").eq(1).addClass("active")
+    },1500)
+    setTimeout(function(){
+        $(".loading").eq(1).removeClass("active")
+        $(".item.p1").addClass("active")
+    },3000)
+    setTimeout(function(){
+        $(".item.p1").removeClass("active")
+        $(".item.p2").addClass("active")
 
-            $(".item").each(function(e,v){
-                if($(v).attr("id") < num){
-                    $(v).addClass("active");
-                }
+    },4000)
+    setTimeout(function(){
+        $(".item.p2").addClass("actives")
+        $(".loading.b7").addClass("actives")
 
-            })
-        }else if(6<num&&num<=57){
-            $(".item").each(function(e,v){
-                if($(v).attr("id") < num){
-                    $(v).addClass("active");
-                    $(v).addClass("actives");
-                }
-            })
+    },5500)
+    setTimeout(function(){
+        $(".item.p3").addClass("active")
+        $(".loading.b7").removeClass("actives")
+    },6500)
+    setTimeout(function(){
+        $(".item.p3").addClass("active")
+        $(".item.p4").addClass("active")
+        // $(".loading.b7").removeClass("actives")
+    },7500)
+    setTimeout(function(){
+        $(".item.p3").addClass("act")
+        $(".item.p4").addClass("act")
+        $(".loading.b9s").addClass("act")
+        setTimeout(function(){
 
-        }
+            $(".loading.b9s").addClass("acs")
+            $(".loading.b9").eq(0).addClass("ac")
 
-    }
+        },1500)
+        setTimeout(function(){
+            $(".loading.b9").eq(0).addClass("acs")
+            $(".loading.b9").eq(1).addClass("ac")
 
-    var t =setInterval(function(){abc()},2000);
+        },2200)
+        setTimeout(function(){
+            $(".loading.b9").eq(1).addClass("acs")
+            $(".loading.b9").eq(2).addClass("ac")
 
-
-    $(".item").each(function(e,v){
-        $(v).css({background:"url('images/0000-"+(e+1)+".jpg') no-repeat center center / cover"}).attr("id",e+1);
-    })
-    var num = 0;
-    $(".item").each(function(e,v){
-
-        if(0<num&&num<6){
-        }else if(6<$(v).attr("id")&&$(v).attr("id")<15){
-                $(v).addClass("at1");
-        }
-    })
+        },2700)
+    },9000)
+    // var t =setInterval(function(){abc()},2000);
 
 
 
